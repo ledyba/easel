ca1a897d107b3821bda57478d7bdfe5cd77e29db
Kaede Fujisaki <kaede@hexe.net>
2016-12-13 12:52:27 +0900

    add test

--- easel_test.go
+++ easel_test.go
@@ -2,10 +2,9 @@ package easel
 
 import (
 	"image"
-	"image/png"
+	_ "image/png"
 	"io/ioutil"
 	"log"
-	"math"
 	"os"
 	"runtime"
 	"testing"
@@ -16,63 +15,18 @@ import (
 const VertexShader = `
 #version 410 core
 layout(location = 0) in vec3 vert;
-out vec2 uv;
 
 void main() {
-	uv = (vert.xy+vec2(1,1))/2.0;
 	gl_Position = vec4(vert, 1);
 }
 `
 const FragmentShader = `
 #version 410
-uniform sampler2D tex;
-uniform vec2 srcSize;
-uniform vec2 scale;
-in vec2 uv;
 layout(location = 0) out vec4 color;
-const float kPI = 3.14159265358979323846264338327950288;
-const int kLobe = 3;
-
-double L(float x) {
-	if (abs(x) <= 0.00000001) {
-		return 1;
-	}
-	float px = kPI * x;
-	double r = double(kLobe) / (double(px) * double(px));
-	r *= sin(px);
-	r *= sin(px / float(kLobe));
-	return r;
-}
 
 void main() {
-  dvec4 sum = vec4(0,0,0,0);
-  vec2 srcPt = srcSize * uv;
-  vec2 base = floor(srcPt) + vec2(0.5, 0.5);
-
-  vec2 pt;
-  dvec4 c;
-  double w;
-  double sumw = 0;
-  vec2 nscale = max(vec2(1,1), 1 / scale);
-  vec2 support = kLobe * nscale;
-  nscale = 1 / nscale;
-  vec2 start = floor(base - support + 0.5);
-  ivec2 contributes = ivec2(support * 2);
-
-	double alpha;
-  for(int dx = 0; dx < contributes.x; dx++) {
-    for(int dy = 0; dy < contributes.y; dy++) {
-      pt = start + vec2(dx, dy);
-      c = dvec4(texture(tex, pt / srcSize));
-      w = L(nscale.x * (pt.x - srcPt.x + 0.5)) * L(nscale.y * (pt.y - srcPt.y + 0.5));
-      sumw += w;
-      sum += c * w;
-    }
-  }
-  sum /= sumw;
-	color = vec4(clamp(sum, dvec4(0,0,0,0), dvec4(1,1,1,1)));
+	color = vec4(1,0,0,1);
 }
-
 `
 
 func init() {
@@ -117,6 +71,85 @@ func TestCreatePalette(t *testing.T) {
 	defer p.Destroy()
 }
 
+func TestCompileProgram(t *testing.T) {
+	var err error
+	startup()
+	defer shutdown()
+	e := NewEasel()
+	e.MakeCurrent()
+	defer e.DetachCurrent()
+	defer e.Destroy()
+	p, err := e.CompileProgram(VertexShader, FragmentShader)
+	if err != nil {
+		t.Fatalf("Failed to compile program. %v", err)
+	}
+	defer p.Destroy()
+	err = p.Use()
+	if err != nil {
+		t.Fatalf("Failed to use program. %v", err)
+	}
+	defer p.Unuse()
+}
+
+func TestCompileInvalidProgram(t *testing.T) {
+	var err error
+	startup()
+	defer shutdown()
+	e := NewEasel()
+	e.MakeCurrent()
+	defer e.DetachCurrent()
+	defer e.Destroy()
+	_, err = e.CompileProgram("", FragmentShader)
+	if err == nil {
+		t.Fatalf("Oops. We succeeded to compile empty shader: %v", err)
+	}
+	_, err = e.CompileProgram(VertexShader, "")
+	if err == nil {
+		t.Fatalf("Oops. We succeeded to compile empty shader: %v", err)
+	}
+}
+
+func TestLoadingTexture(t *testing.T) {
+	var err error
+	startup()
+	defer shutdown()
+	e := NewEasel()
+	e.MakeCurrent()
+	defer e.DetachCurrent()
+	defer e.Destroy()
+
+	freader, err := os.Open("test-images/momiji.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer freader.Close()
+	bytes, err := ioutil.ReadAll(freader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tex, _, err := e.LoadTexture2D(bytes)
+	if err != nil {
+		t.Fatalf("Could not create texure: \n** Message **\n%v", err)
+	}
+	defer tex.Destroy()
+}
+
+func TestFailingLoadingTexture(t *testing.T) {
+	var err error
+	startup()
+	defer shutdown()
+	e := NewEasel()
+	e.MakeCurrent()
+	defer e.DetachCurrent()
+	defer e.Destroy()
+
+	_, _, err = e.LoadTexture2D([]byte{})
+	if err == nil {
+		t.Fatalf("Oops. Succeeded to loading empty binary...")
+	}
+}
+
 func TestRender(t *testing.T) {
 	var err error
 	startup()
@@ -146,29 +179,75 @@ func TestRender(t *testing.T) {
 	defer prog.Unuse()
 	defer prog.Destroy()
 
-	/**** Render Image ****/
-	freader, err := os.Open("test-images/momiji.png")
+	p.vertexArray.bind()
+	indecies, err := p.AttachArrayIndexBuffer([]uint16{0, 1, 3, 2, 3, 0})
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("Could not bind array indecies: \n** Message **\n%v", err)
 	}
-	defer freader.Close()
-	bytes, err := ioutil.ReadAll(freader)
-	if err != nil {
-		log.Fatal(err)
-	}
+	buf, err := p.MakeArrayBuffer([]float32{
+		-1, -1, 0,
+		1, -1, 0,
+		-1, 1, 0,
+		1, 1, 0,
+	})
 
-	tex, src, err := e.LoadTexture2D(bytes)
 	if err != nil {
 		t.Fatalf("Could not create texure: \n** Message **\n%v", err)
 	}
-	defer tex.Destroy()
+	err = p.BindArrayAttrib(buf, indecies, "vert", 3, 0, 0)
+	if err != nil {
+		t.Fatalf("Could not bind array attrib: \n** Message **\n%v", err)
+	}
 
-	p.BindUniformf("srcSize", 2, []float32{
-		float32(src.Bounds().Dx()), float32(src.Bounds().Dy())})
-	p.BindUniformf("scale", 2, []float32{
-		float32(math.Max(1.0, float64(256.0)/float64(src.Bounds().Dx()))),
-		float32(math.Max(1.0, float64(256.0)/float64(src.Bounds().Dy()))),
-	})
+	img, err := p.Render(image.Rect(0, 0, 256, 256))
+	if err != nil {
+		t.Fatalf("Could not execute: \n** Message **\n%v", err)
+	}
+
+	r, g, b, a := img.At(0, 0).RGBA()
+	if r == 65535 && g == 0 && b == 0 && a == 65535 {
+	} else {
+		t.Errorf("Failed to execute shader. Expected red pixel, but got (%d,%d,%d,%d)", r, g, b, a)
+	}
+}
+
+func TestBindUniform(t *testing.T) {
+	var err error
+	startup()
+	defer shutdown()
+	e := NewEasel()
+	e.MakeCurrent()
+	defer e.DetachCurrent()
+	defer e.Destroy()
+	p, err := e.NewPalette()
+	if err != nil {
+		t.Fatalf("Could not creating palette: \n** Message **\n%v", err)
+	}
+	err = p.Bind()
+	if err != nil {
+		t.Fatalf("Could not bind palette: \n** Message **\n%v", err)
+	}
+	defer p.Unbind()
+	defer p.Destroy()
+	prog, err := e.CompileProgram(VertexShader, `
+		#version 410
+		layout(location = 0) out vec4 color;
+		uniform vec4 c1;
+		uniform ivec4 c2;
+
+		void main() {
+			color = c1 * vec4(c2);
+		}
+`)
+	if err != nil {
+		t.Fatalf("Failed to compile program. %v", err)
+	}
+	p.AttachProgram(prog)
+	prog.Use()
+	defer prog.Unuse()
+	defer prog.Destroy()
+	p.BindUniformf("c1", 4, []float32{1, 1, 0, 1})
+	p.BindUniformi("c2", 4, []int32{0, 1, 0, 1})
 
 	p.vertexArray.bind()
 	indecies, err := p.AttachArrayIndexBuffer([]uint16{0, 1, 3, 2, 3, 0})
@@ -190,17 +269,14 @@ func TestRender(t *testing.T) {
 		t.Fatalf("Could not bind array attrib: \n** Message **\n%v", err)
 	}
 
-	p.BindTexture("tex", tex)
-
 	img, err := p.Render(image.Rect(0, 0, 256, 256))
 	if err != nil {
 		t.Fatalf("Could not execute: \n** Message **\n%v", err)
 	}
-	file, err := os.Create("test.out.png")
-	if err != nil {
-		t.Error(err)
+
+	r, g, b, a := img.At(0, 0).RGBA()
+	if r == 0 && g == 65535 && b == 0 && a == 65535 {
+	} else {
+		t.Errorf("Failed to execute shader. Expected green pixel, but got (%d,%d,%d,%d)", r, g, b, a)
 	}
-
-	png.Encode(file, img)
-
 }
