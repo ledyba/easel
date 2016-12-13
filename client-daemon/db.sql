b33d8201cf0883951ea61168d03af736b80c2619
Kaede Fujisaki <kaede@hexe.net>
2016-12-13 17:54:03 +0900

    uploaded -> updated

--- client-daemon/db.sql
+++ client-daemon/db.sql
@@ -6,7 +6,7 @@ CREATE TABLE `resample_requests` (
 `dst_height` int(10) unsigned NOT NULL,
 `dst_quality` double unsigned NOT NULL,
 `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`uploaded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+`updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 `status` tinyint(1) unsigned NOT NULL COMMENT '0=enqueued, 1=in progress, 2=done, 3=error',
 `message` text NOT NULL,
 PRIMARY KEY (`id`)
